31362eb8862480aa882077188f0371b36e10ee05
Leanne Springthorpe <lrteeter@gmail.com>
2019-04-09 13:48:28 -0700

    Add files via upload

---
+++ world_quest_scanner.py
@@ -0,0 +1,46 @@
+import urllib2, re, smtplib, ssl, time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def scan_active_world_quests():
+    sender_email = 'myemail@gmail.com'
+    receiver_email = 'myemail@gmail.com'
+    password = 'mypassword'
+    email_subject = ''
+    email_body = ''
+
+    strings = ['Sabertron', 'Whiplash']
+    source = urllib2.urlopen('https://www.wowhead.com/world-quests/bfa/na').read()
+
+    for string in strings:
+        string = string.capitalize()
+        if re.search(string, source, flags=re.IGNORECASE):
+            print(string + ' found on ' + time.ctime())
+            email_body += '<p>' + string + ' found on ' + time.ctime() + '!</p>'
+            if len(email_subject) > 0:
+                email_subject += ', ' + string
+            else: email_subject = string
+
+        else:
+            print('Scan finished unsuccessfully for ' + string + ' on ' + time.ctime())
+
+    if len(email_body) > 0:
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = email_subject
+        msg['From'] = sender_email
+        msg['To'] = receiver_email
+
+        body = '<html><body>' + email_body + '</body></html>'
+        html = MIMEText(body, 'html')
+
+        msg.attach(html)
+        s = smtplib.SMTP_SSL('smtp.gmail.com')
+        s.login(sender_email, password)
+
+        s.sendmail(sender_email, receiver_email, msg.as_string())
+        s.quit()
+
+while True:
+    six_hours = 21600
+    scan_active_world_quests()
+    time.sleep(six_hours)
